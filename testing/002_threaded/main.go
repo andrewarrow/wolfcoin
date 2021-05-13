1f97fa7c301e5af91625462f74b0bff0db25e797
Andrew Arrow <oneone@gmail.com>
2021-05-12 21:05:20 -0700

    thread

--- testing/002_threaded/main.go
+++ testing/002_threaded/main.go
@@ -5,12 +5,12 @@ import (
 	"time"
 )
 
-var gossip []Tx = []Tx{}
-var rejects []Tx = []Tx{}
+var nodes = []Node{}
 
 type Node struct {
-	Id    int
-	Books map[string]int
+	Id     int
+	Books  map[string]int
+	Gossip chan Tx
 }
 
 type Tx struct {
@@ -24,27 +24,49 @@ func NewNode(id int) Node {
 	node := Node{}
 	node.Id = id
 	node.Books = map[string]int{}
+	node.Books["ABC"] = 100
+	node.Gossip = make(chan Tx, 1024)
+	go node.ListenToGossip()
+	go node.ListenToRejects()
 	return node
 }
 
 func (n Node) ListenToGossip() {
-	for {
-		fmt.Println(n.Id, "ListenToGossip")
+	for g := range n.Gossip {
+		fmt.Println(n.Id, "ListenToGossip", g)
 		time.Sleep(time.Second)
+	}
+}
+func (n Node) ListenToRejects() {
+	for {
+		fmt.Println(n.Id, "ListenToRejects")
+		time.Sleep(time.Second)
+	}
+}
+
+func (n Node) AddTx(id int, from, to string, amount int) {
+	n.Books[from] -= amount
+	n.Books[to] += amount
+	tx := Tx{id, from, to, amount}
+	for _, other := range nodes {
+		if other.Id == n.Id {
+			continue
+		}
+		other.Gossip <- tx
 	}
 }
 
 func main() {
 	node1 := NewNode(1)
-	node1.Books["ABC"] = 100
 	node2 := NewNode(2)
-	node2.Books["ABC"] = 100
 	node3 := NewNode(3)
-	node3.Books["ABC"] = 100
+
+	nodes = []Node{node1, node2, node3}
 
 	fmt.Println(node1, node2, node3)
 
-	go node1.ListenToGossip()
+	node2.AddTx(123, "ABC", "EFG", 100)
+	node3.AddTx(124, "ABC", "XYZ", 100)
 
 	for {
 		time.Sleep(time.Second)
